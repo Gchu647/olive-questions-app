382061e736ee57ba24144f33d1b507370eac6eb5
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-10-14 22:34:27 -1000

    Updates

---
+++ precache-manifest.ee86f96933ea0a8c7cf3e81ababfe68e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc3c3cdd01b20d16c165a7573949664",
+    "url": "/olive-questions-app/index.html"
+  },
+  {
+    "revision": "d2603eb5185fd93b547c",
+    "url": "/olive-questions-app/static/css/main.87d065f1.chunk.css"
+  },
+  {
+    "revision": "6e5c2a018dd21c64cd73",
+    "url": "/olive-questions-app/static/js/2.a7b36eb6.chunk.js"
+  },
+  {
+    "revision": "d2603eb5185fd93b547c",
+    "url": "/olive-questions-app/static/js/main.0b61df89.chunk.js"
+  },
+  {
+    "revision": "bd8b4664babc5ee423ef",
+    "url": "/olive-questions-app/static/js/runtime-main.6f49be59.js"
+  },
+  {
+    "revision": "039db90a6ca38f9f0a5b488daf522f9d",
+    "url": "/olive-questions-app/static/media/finish-bg.039db90a.png"
+  },
+  {
+    "revision": "f22678fee2d962ce40ccb7c4a1a7ff49",
+    "url": "/olive-questions-app/static/media/question-bg.f22678fe.png"
+  }
+]);
